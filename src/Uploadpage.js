defdce52a15991ac16043feee18438fb42aea24c
Sriyanshjain <shreyanshjain036@gmail.com>
2021-10-12 11:42:23 +0530

    routing added

--- src/Uploadpage.js
+++ src/Uploadpage.js
@@ -1,5 +1,5 @@
 import { Grid, Typography } from "@material-ui/core";
-import React from "react";
+import React,{useState} from "react";
 import Contact from './assets/Contacts upload.svg';
 
 
@@ -29,22 +29,35 @@ const useStyles = makeStyles({
   },
 
     })
-function Uploadpage(){
 
+function Uploadpage(){
+    
+    const [selectedFile, setSelectedFile] = useState();
+	const [isFilePicked, setIsFilePicked] = useState(false);
+    const changeHandler = (event) => {
+		setSelectedFile(event.target.files[0]);
+		setIsFilePicked(true);
+	};
+
+	const handleSubmission = () => {
+	};
     const classes=useStyles();
     return (<Grid container direction="column">
         
-        <Grid item xs={3} >
-            <Typography style={{marginTop:"10%",padding:'2%', color:'#262261'}}>
-                BlueMind | <span style={{fontWeight:'bold'}}>Fact Finding</span>
+        <Grid container direction="row" alignItems="center" >
+            <Grid item xs={7} sm={5}>
+            <Typography style={{margin:"5% auto",padding:'2%', color:'#262261'}}>
+                BlueMind | <span style={{fontWeight:'bold'}}>Contacts Finding</span>
             </Typography>
-        </Grid>
-        <Grid item xs={3}>
-            <Typography style={{marginTop:"10%",padding:'2%', color:'#262261'}}>
+            </Grid>
+            </Grid>
+           <Grid container direction="row" alignItems="center" style={{justifyContent:'flex-start'}}>
+               <Grid item xs={5} sm={5}>
+            <Typography style={{margin:"5% auto",padding:'2%', color:'#262261'}}>
                 Import to BlueMind
             </Typography>
-        </Grid>
-        
+            </Grid>
+            </Grid>
         <Grid container direction="row" alignItems="center">
            <Grid item xs={2}/>
             <Grid item xs={8}>
@@ -53,28 +66,27 @@ function Uploadpage(){
            <Grid item xs={2}/>
         </Grid>
         <Grid container direction="row" alignItems="center"  style={{marginTop:'5%'}}>
-           <Grid item xs={4}/>
-            <Grid item xs={4}>
-                {/* <ContactCard/> */}
-  <Loader/>
+           <Grid item xs={3} sm={3}/>
+            <Grid item xs={6} sm={6}>
+               {isFilePicked?<Loader/>:<ContactCard/>}
            </Grid>
-           <Grid item xs={4}/>
+           <Grid item xs={3} sm={3}/>
         </Grid>
 
         <Grid container direction="row" alignItems="center" style={{justifyContent:'center'}}>
           
             <Grid item xs={12} sm={8}>
-            <Typography style={{marginTop:"5%",padding:'2%',fontSize:'30px',fontFamily: 'Open Sans, sans-serif', color:'#a8a6c0'}}>
+            <Typography style={{marginTop:"5%",padding:'2%',fontSize:'2rem',fontFamily: 'Open Sans, sans-serif', color:'#a8a6c0'}}>
                 File from computer
             </Typography>
            </Grid>
            <Grid item xs={12} sm={8}>
-            <Typography style={{marginTop:"2%",padding:'1%',fontSize:'20px',fontFamily: 'Open Sans, sans-serif', color:'#a8a6c0'}}>
+            <Typography style={{marginTop:"2%",padding:'1%',fontSize:'1.3rem',fontFamily: 'Open Sans, sans-serif', color:'#a8a6c0'}}>
                Upload any CSV,XLS or XLSX files with your data.
             </Typography>
            </Grid>
            <Grid item xs={12} sm={8}>
-            <Typography style={{marginTop:"2%",padding:'1%',fontSize:'20px',fontFamily: 'Open Sans, sans-serif',fontWeight:'bold', color:'#262261'}}>
+            <Typography style={{marginTop:"2%",padding:'1%',fontSize:'1.3rem',fontFamily: 'Open Sans, sans-serif',fontWeight:'bold', color:'#262261'}}>
                Need help getting started ?
             </Typography>
            </Grid>
@@ -82,17 +94,17 @@ function Uploadpage(){
         <Grid container direction="row" style={{marginTop:'2%'}}>
             
             <Grid item xs={4}>
-            <Typography style={{padding:'1%',fontSize:'20px',fontFamily: 'Open Sans, sans-serif',fontWeight:'bold'}}>
+            <Typography style={{padding:'1%',fontSize:'1.3rem',fontFamily: 'Open Sans, sans-serif',fontWeight:'bold'}}>
                <a className={classes.linkStyle} href="#">View import guide</a>
             </Typography>
             </Grid>
             <Grid item xs={5}>
-            <Typography style={{padding:'1%',fontSize:'20px',fontFamily: 'Open Sans, sans-serif',fontWeight:'bold'}}>
+            <Typography style={{padding:'1%',fontSize:'1.3rem',fontFamily: 'Open Sans, sans-serif',fontWeight:'bold'}}>
                <a className={classes.linkStyle} href='#'> Download sample spreadsheet</a>
             </Typography>
             </Grid>
             <Grid item xs={3}>
-            <Typography style={{padding:'1%',fontSize:'20px',fontFamily: 'Open Sans, sans-serif',fontWeight:'bold'}}>
+            <Typography style={{padding:'1%',fontSize:'1.3rem',fontFamily: 'Open Sans, sans-serif',fontWeight:'bold'}}>
                <a className={classes.linkStyle}  href="#">View the FAQ</a>
             </Typography>
             </Grid>
@@ -105,18 +117,21 @@ function Uploadpage(){
             </Grid>
             <Grid item xs={6}/>
             <Grid item xs={3}>
+                <form>
             <input
         accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
         className={classes.input}
         id="contained-button-file"
         multiple
         type="file"
+        onChange={changeHandler}
       />
       <label htmlFor="contained-button-file">
-        <Button variant="contained" className={classes.Buttonstyle} component="span">
+        <Button variant="contained" className={classes.Buttonstyle} component="span" onClick={handleSubmission}>
           import
         </Button>
       </label>
+      </form>
            </Grid> 
         </Grid>
         
--- src/contactcard.js
+++ src/contactcard.js
@@ -4,16 +4,16 @@ import Radio from '@material-ui/core/Radio';
 import ContactImg from './assets/contact_image.svg';
 
 function ContactCard(){
-return(  <Card style={{width:'80%',margin:'5% 10%',borderRadius:'15px',border:'1.5px solid rgb(38,34,97,0.4)'}}>
+return(  <Card style={{width:'70%',margin:'5% auto' ,borderRadius:'1rem',border:'1.5px solid rgb(38,34,97,0.4)'}}>
 <Radio
 checked={true}
 color="primary"
 name="radio-button-demo"
 size="small"
-style={{marginLeft:'80%'}}
+style={{marginLeft:'75%'}}
 />
 <img
-src={ContactImg} style={{width:'40%',height:'250px'}}/>
+src={ContactImg} style={{width:'40%',height:'16rem'}}/>
 </Card>)
 }
 
